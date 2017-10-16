5f7bde6f851ce657cbd9f28e49403abf218d8980
olujuide Oyewole <ooye359@mail.midland.edu>
2017-10-16 18:55:25 -0500

    Initial commit

---
+++ src/Main.groovy
@@ -0,0 +1,15 @@
+Room room1d = new Room(
+        roomld: 1,
+        title: "Room 1",
+        description: "This is room 1"
+)
+Room room2 = new Room(
+        roomld: 2,
+        title: "Room 2",
+        description: "This is room 2"
+)
+Room room3 = new Room(
+        roomld: 3,
+        title: "Room1",
+        description: "This is room 3"
+)
---
+++ src/Room.groovy
@@ -0,0 +1,5 @@
+class Room {
+   Integer roomld
+   String title
+   String description
+}
